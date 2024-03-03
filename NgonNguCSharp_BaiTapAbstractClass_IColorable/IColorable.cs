725befeec1f1bf5045d0d0e860cb4dd4bc1c57cc
Lâm Bình Hoàn <hoanlb1988@gmail.com>
2024-03-03 22:54:42 +0700

    icolorable

---
+++ NgonNguCSharp_BaiTapAbstractClass_IColorable/IColorable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgonNguCSharp_BaiTapAbstractClass_IColorable
+{
+    public interface IColorable
+    {
+        string HowToColor();
+    }
+}
---
+++ NgonNguCSharp_BaiTapAbstractClass_IColorable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgonNguCSharp_BaiTapAbstractClass_IColorable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+            shape = new Square();
+
+            IColorable iColorable = (Square)shape;
+
+            Console.WriteLine("Square 's color is: " + iColorable.HowToColor());
+
+            Console.ReadKey();
+        }
+    }
+}
